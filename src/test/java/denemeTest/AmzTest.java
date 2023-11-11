0608b4f17b43aa7eaae4dcd5bacacd1fcdfd63ec
Haldun35 <tsmnc054@gmail.com>
2023-11-11 11:45:54 +0100

    AmazonTest

--- src/test/java/denemeTest/AmzTest.java
+++ src/test/java/denemeTest/AmzTest.java
@@ -29,6 +29,9 @@ public class AmzTest {
     ArrayList<String> arr = new ArrayList<>();
 
     By lLocator = By.cssSelector(".s-suggestion-container div");
+    By lSearch = By.cssSelector("#nav-search-submit-button");
+    By lInfos = By.xpath("//h2[@class='a-size-mini a-spacing-none a-color-base s-line-clamp-4']");
+    By lRezensionen = By.xpath("//a[@class='a-popover-trigger a-declarative']");
 
 
     public AmzTest() {
@@ -40,24 +43,35 @@ public class AmzTest {
 
 
     @Test
-    public void aaa() {
+    public void aaa() throws InterruptedException {
         driver.get(url);
         wait.until(ExpectedConditions.titleContains("Amazon")); // assert title
         sendKey(getInputByLocator("Suche Amazon.de"), "wireless");
-       /* checkDropProducts("wireless");
+        checkDropProducts("wireless");
+        click(lSearch);
+        checkTheInfos("Wireless", "Kabellos");
+
+        /* checkDropProducts("wireless");
         System.out.println(arr);*/
         //wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.cssSelector(".s-suggestion-container div"),0));
         //Assert.assertTrue(locators.products.size()>0);
 
-        long num = driver.findElements(lLocator).stream().filter(e -> !e.getText().toLowerCase().contains("wireless")).count();
-        Assert.assertEquals(num,0);
 
     }
 
+    public void sleep(int num) throws InterruptedException {
+        try {
+
+            Thread.sleep(num);
+
+        }catch (InterruptedException e){
+
+        }
+    }
 
     @AfterTest()
     public void afterTest() {
-        //driver.quit();
+        driver.quit();
     }
 
     public void sendKey(By locator, String text) {
@@ -96,26 +110,41 @@ public class AmzTest {
 
     }
 
-    public void checkDropProducts() {
+   /* public void checkDropProducts() {
         for (WebElement product : locators.products) {
             System.out.println(product.getText());
         }
-    }
+    }*/
 
     public void checkDropProducts(String pName) {
 
-
-        for (WebElement element : locators.products) {
-            if (element.getText().contains(pName)) {
-                arr.add(element.getText());
-                System.out.println(element.getText());
-            }
-
-        }
-
-
+        long num = driver.findElements(lLocator).stream().filter(e -> !e.getText().toLowerCase().startsWith(pName)).count();
+        Assert.assertEquals(num, 0);
     }
 
+    public void checkTheInfos(String text1, String text2) {
+
+        String texts = ""+ text1 + " | " + text2;
+
+       long num = driver.findElements(lInfos).stream().filter(e -> !e.getText().toLowerCase().contains(texts)).count();
+        System.out.println(num);
+    }
+    public void checkRezensionWithProduct(){
+
+        int rezNums=0;
+
+        List<WebElement> rezensionen = driver.findElements(lRezensionen);
+
+        for (WebElement rezension : rezensionen) {
+
+            if (rezension.getText().toLowerCase().contains("sternen")) {
+                rezNums++;
+            } else {
+
+            }
+        }
+
+    }
 
 }
 
